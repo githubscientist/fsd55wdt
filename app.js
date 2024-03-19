03060e784e40bd84b63f88f8c70f8916f230074e
githubscientist <sathish.datascientist@gmail.com>
2024-03-19 11:24:36 +0530

    morgan for logger and cookie-parser added and configured

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 // require express dependency
 const express = require('express');
-
 // import cors
 const cors = require('cors');
-
+// require the user routes
 const userRouter = require('./routes/userRoutes');
+// require morgan for logging
+const morgan = require('morgan');
+// require cookie-parser for parsing cookies
+const cookieParser = require('cookie-parser');
+
 
 // create an express application
 const app = express();
 
+// middlewares
 // use the cors middleware
 app.use(cors());
-
+// use morgan for logging
+app.use(morgan('dev'));
+// use cookie-parser
+app.use(cookieParser());
 // parse the request body as JSON
 app.use(express.json());
 
+// routes middlewares
 // define the endpoints
 app.use('/api/users', userRouter);
 
